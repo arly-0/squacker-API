4379a1c860a60eecca7376dcfa423801f9f9cbb6
Artur <arturworkit@gmail.com>
2022-10-23 00:13:53 +0300

    Enable timestamps on user model

--- models/auth-models/user-model.js
+++ models/auth-models/user-model.js
@@ -1,11 +1,10 @@
 import {Schema, model} from 'mongoose'
 
-//TODO: timestamps: true
 const UserSchema = new Schema({
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
     isActivated: {type: Boolean, default: false},
     activationLink: {type: String}
-})
+}, {timestamps: true})
 
 export default model('User', UserSchema)
