17516d8b6ce9a791bd95e0fdd0fbcaeee40cd4e6
Artur <arturworkit@gmail.com>
2022-10-23 00:13:34 +0300

    Update lap and session models structure

--- models/session-models/lap-model.js
+++ models/session-models/lap-model.js
@@ -1,8 +1,10 @@
 import {Schema, model} from 'mongoose'
 
 const LapSchema = new Schema({
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    session: {type: Schema.Types.ObjectId, ref: 'Session', required: true},
     lap_number: {type: Number, required: true},
     lap_time: {type: String, required: true}
-})
+}, {timestamps: true})
 
 export default model('Lap', LapSchema)
--- models/session-models/session-model.js
+++ models/session-models/session-model.js
@@ -1,15 +1,15 @@
 import {Schema, model} from 'mongoose'
 
 const SessionSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-    date: {type: Date, required: true},
-    //laps: [{type: Schema.Types.ObjectId, ref: 'Lap', required: true}],
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    date: {type: Date, required: true, default: () => null},
     laps: [{type: new Schema({
             lap_number: {type: Number, required: true},
             lap_time: {type: String, required: true}
         }), required: true}],
     best_lap: {type: Date, default: () => null},
     avg_lap: {type: Date, default: () => null},
+    track_length: {type: Number, default: () => null},
     note: {type: String, trim: true, maxLength: 150, default: null},
     wet: {type: Boolean, default: () => false}
 }, {timestamps: true})
