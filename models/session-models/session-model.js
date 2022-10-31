5a360151696828b6b04d86159f4f4a409d417e80
Artur <arturworkit@gmail.com>
2022-10-31 18:46:45 +0200

    Fix default value error in session model

--- models/session-models/session-model.js
+++ models/session-models/session-model.js
@@ -2,7 +2,7 @@ import {Schema, model} from 'mongoose'
 
 const SessionSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-    date: {type: Date, required: true, default: () => null},
+    date: {type: Date, default: () => null},
     laps: [{type: new Schema({
             lap_number: {type: Number, required: true},
             lap_time: {type: String, required: true}
@@ -10,7 +10,7 @@ const SessionSchema = new Schema({
     best_lap: {type: String, default: () => null},
     avg_lap: {type: String, default: () => null},
     track_length: {type: Number, default: () => null},
-    note: {type: String, trim: true, maxLength: 150, default: null},
+    note: {type: String, trim: true, maxLength: 150, default: () => null},
     wet: {type: Boolean, default: () => false}
 }, {timestamps: true})
 
