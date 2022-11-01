73533bc9ef174f419ef4068d72bd9c5efc731c9e
Artur <arturworkit@gmail.com>
2022-11-01 21:48:54 +0200

    Add note field length validation

--- controllers/session-controller.js
+++ controllers/session-controller.js
@@ -1,8 +1,14 @@
 import SessionService from "../services/session-service/session-service.js"
+import {validationResult} from "express-validator";
+import ApiError from "../exceptions/api-error.js";
 
 export default class SessionController {
     static async create(req, res, next) {
         try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Validation error', errors.array()))
+            }
             const {user, date, laps, best_lap, avg_lap, track_length, note, wet} = req.body
             const session = await SessionService.create(user, date, laps, best_lap, avg_lap, track_length, note, wet)
             return res.status(201).json(session)
@@ -13,6 +19,10 @@ export default class SessionController {
 
     static async update(req, res, next) {
         try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Validation error', errors.array()))
+            }
             const newSession = req.body
             const session_id = req.params.session_id
             const updatedSession = await SessionService.update(session_id, newSession)
--- router/session.js
+++ router/session.js
@@ -1,11 +1,12 @@
 import Router from 'express'
 import SessionController from "../controllers/session-controller.js"
 import authMiddleware from "../middlewares/auth-middleware.js";
+import {body} from "express-validator";
 
 const sessionRouter = new Router()
 
-sessionRouter.post('/', authMiddleware, SessionController.create)
-sessionRouter.patch('/:session_id', authMiddleware, SessionController.update)
+sessionRouter.post('/', body('note').isLength({ max: 150 }), authMiddleware, SessionController.create)
+sessionRouter.patch('/:session_id', body('note').isLength({ max: 150 }), authMiddleware, SessionController.update)
 sessionRouter.delete('/:session_id', authMiddleware, SessionController.delete)
 
 export default sessionRouter
